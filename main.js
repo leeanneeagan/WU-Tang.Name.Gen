8d8fe6861dc3b8a1a6c7dee96d84f28df08919ee
leeanneeagan <leeanne.eagan@gmail.com>
2025-10-12 14:23:25 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,53 @@
+// Define survey questions
+
+//  Pick 5 questions that give personality, style, or vibes to the name
+
+// Favorite element or nature thing: Fire, Water, Wind, Earth, Lightning
+
+//  Favorite animal: Tiger, Wolf, Eagle, Snake, Panther
+
+//  Favorite color or vibe: Red, Black, Gold, Silver, Shadow
+
+// Favorite city or place
+
+// Pick a style word: Master, Ghost, Kid, Assassin, Dragon
+
+
+
+// const elements = {
+//   Fire: ["Flame", "Blaze", "Inferno"],
+//   Water: ["Wave", "Tide", "Splash"],
+//   Wind: ["Storm", "Breeze", "Cyclone"],
+//   Earth: ["Rock", "Stone", "Mud"],
+//   Lightning: ["Bolt", "Thunder", "Shock"]
+// };
+
+// const animals = {
+//   Tiger: ["Tiger", "Panther", "Lion"],
+//   Wolf: ["Wolf", "Hound", "Jackal"],
+//   Eagle: ["Eagle", "Hawk", "Falcon"],
+//   Snake: ["Serpent", "Cobra", "Viper"],
+//   Panther: ["Shadow", "Night", "Stealth"]
+// };
+
+
+
+
+
+// function pickRandomWord(list) {
+//   return list[Math.floor(Math.random() * list.length)];
+// }
+// . Combine words to create a Wu-Tang-style name
+
+// Typically, Wu-Tang names are 1-3 words.
+
+//combine element + animal + style, or color + place + word, etc.
+
+//
+
+// const wuTangName = pickRandomWord(elements[userAnswer1]) + " " +
+//                    pickRandomWord(animals[userAnswer2]) + " " +
+//                    pickRandomWord(styleWords[userAnswer5]);
+
+
+//                
